d701bf73575f84687191094a8202587894b9cda0
fismaili <f.ismaili@seeu.edu.mk>
2025-10-16 10:49:44 +0200

    Week 3

---
+++ resources/views/jobs/jobs.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+   <h1><? echo " <h1> Hello from php </h1>"; ?></h1> 
+       <h1> {{$title }}</h1>
+    <ul>
+    <ul>
+      @foreach ($jobs as $job)
+      <li>{{$job}}</li>
+      @endforeach
+    </ul>
+    </ul>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/jobs', function () {
+    return view('jobs.jobs');
+});
+
+// Route::get('/jobs', function () {
+//     $title = 'Avail,dhjhfdghvj';
+//     $jobs = [
+//         'Software Engineer',
+//         'Web Developer',
+//         'Data Scientist',
+//     ];
+//     return view('jobs.jobs',compact('title', 'jobs'));
+// });
+
+
