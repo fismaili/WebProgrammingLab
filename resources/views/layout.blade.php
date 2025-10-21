c9ac774492b2689acc07ce66ea072db3cb78b7e2
fismaili <f.ismaili@seeu.edu.mk>
2025-10-21 22:52:50 +0200

    adding partial - navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,7 +7,7 @@
   </head>
 
   <body class="bg-gray-100">
-    <h1>My App</h1>
+    @include('partials.navbar')
     <main class="container mx-auto p-4 mt-4">@yield('content')</main>
   </body>
 </html>
